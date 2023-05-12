bc71769710b8aa259493dde5c26471d9588d5e2d
ArinIvanova <132432375+ArinIvanova@users.noreply.github.com>
2023-05-12 18:38:16 +0300

    вывод в консоль фразы(плагины заархивированы)

---
+++ index1.js
@@ -0,0 +1,6 @@
+console.log('Hellow world!');
+
+const myFirstvARIABLE = 10
+
+console.log(myFirstvARIABLE)
+
---
+++ webpack.config1.js
@@ -0,0 +1,42 @@
+// Импортируем плагины, вспомогательные библиотеки
+const path = require('path');
+const EncodingPlugin = require('webpack-encoding-plugin');
+const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
+
+module.exports = {
+    // тип сборки - для разработки или в продакшн
+    mode: process.env.NODE_ENV || 'development',
+    
+    // "точка входа" в приложение. Это коренвой js-файл функционала, к нему подключаются остальные файлы
+    entry: {
+        'core': './js1/index1.js'
+    },
+    
+    // директория, в которой будет лежать build (наша сборка)
+    output: {
+        path: path.resolve(__dirname, './js1/build1/'),
+        filename: '[name].bundle1.js',
+    },
+    
+    module: {
+        // правила для трансформации файлов разных расширений
+        rules: [
+            // `js`-файлы должны обработаться спецальным лоадером, который преобразует "новый" код в "старый"
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: path.resolve(__dirname, './node_modules/')
+            },
+           
+        ]
+    },
+    
+    // Подключаем плагины для работы с языками, кодировками и т.д.
+    plugins: [
+        new EncodingPlugin({
+            encoding: 'cp-1251'
+        }),
+        new MomentLocalesPlugin({
+            localesToKeep: ['ru'],
+        }),
+    ]}
