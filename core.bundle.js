f42a541f5dcba7993693f8cc623fd81e1a34aa90
ArinIvanova <132432375+ArinIvanova@users.noreply.github.com>
2023-05-12 13:30:08 +0300

    Delete core.bundle.js

--- core.bundle.js
+++
@@ -1,1 +0,0 @@
-(()=>{var t={attr1:1,attr2:2,attr3:3};watch(t,["attr2","attr3"],(function(){alert("attr2 или attr3 изменены!")})),t.attr2=50;const r={CLOSED:0,FIRST_OPENDE:1,SECOND_OPENED:2,THIRD_OPENED:3},a={currentModalType:r};document.querySelector("#first-opener").addEventListener("click",(()=>{a.currentModalType=r.FIRST_OPEND}))})();
